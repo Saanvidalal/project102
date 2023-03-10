0bb9ed1d97e9baf518e65de7daa3b62a3828f244
Saanvidalal <97969332+Saanvidalal@users.noreply.github.com>
2023-03-10 17:19:19 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+from_dir = "C:/Users/ADMIN/Downloads/C102_assets-main"
+to_dir = "D:/output102"
+list_of_files=os.listdir(from_dir)
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+    if extension=="":
+        continue
+    if extension in [".doc",".docs"]:
+        path1=from_dir+"/"+file_name
+        path2=to_dir+"/"+"document_Files"
+        path3=to_dir+"/"+"document_Files"+"/"+file_name
+        if os.path.exists(path2):
+            shutil.move(path1,path3)
+        else:    
+            os.makedirs(path2)
+            shutil.move(path1, path3)
